0e6dba545d60aac459695a120945b4a3958902bf
Thom Parkin <Thom.Parkin@webembly.com>
2013-11-10 02:08:14 +0000

    Initial Commit on Nitrous Box

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enderstore::Application.config.secret_token = '041e4d9866228ba521b678fdad4eb196c6addc5618a5b10863ca188007da33e43eaa6e12db235d856f303575df0dd697dbc5fc9d9849cc15a629eb18342da1cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Enderstore::Application.config.session_store :cookie_store, key: '_enderstore_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Enderstore::Application.config.session_store :active_record_store
