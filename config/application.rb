086d78adc2cc53cc5d6cc0465a8c7ec19c091da5
Thom Parkin <Thom.Parkin@Websembly.com>
2014-02-03 14:03:59 +0000

    For Heroku the  must be set to true.  See github.com/sferik/rails_admin/issues/1046

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,8 @@ module Enderstore
 
    # Enable the asset pipeline                                                                                                                                                                                                                                         
     config.assets.enabled = true                                                                                                                                                                                                                                        
-    config.assets.initialize_on_precompile = false                                                                                                                                                                                                                      
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile += ['bootstrap.css']
  
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
