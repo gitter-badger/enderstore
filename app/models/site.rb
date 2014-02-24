2946fe3ee1e3752650b2ae1073ca68c9c2d526ed
Thom Parkin <Thom.Parkin@Websembly.com>
2014-02-24 02:49:12 +0000

    Fixed nil as empty string (in sqlite3) versus nil as a true NULL in the PostgreSQL database

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,5 @@
 class Site < ActiveRecord::Base
   attr_accessible :title, :content, :name, :url, :domain
-  
+
+  #url MUST BE set to NULL in the database for each unclaimed subdomain.  Although an empty string works with sqlite3 (in development), the Postgresql is more particular
 end
